d0e7ebaa03802486927893ad74b12b2bff323f5b
Mohsen Keshavarz <m.keshavarz@fanaptelecom.net>
2026-02-17 17:52:23 +0330

    update

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react"
+
 export default function Api(){
 
     //https://jsonplaceholder.typicode.com/posts
+    let [posts,setPost] = useState([])
+    useEffect(
+        () => {
+            fetch("https://jsonplaceholder.typicode.com/posts")
+            .then( response => response.json())
+            .then( data =>setPost(data))
+            .catch( error => console.log(error))
+        }
+        , []
+    )
+
+    if(posts.length==0){
+        return <h2>داده های در حال بارگزاری هستند</h2>
+    }
+
     return(
-        <h1>salam</h1>
+        <div>
+        <h1>لیست پست ها</h1>
+        <ul>
+            {
+                posts.map(
+                    (item) => <li key={item.id}>{item.title}</li>
+                )
+            }
+        </ul>
+
+
+        </div>
     )
 }
